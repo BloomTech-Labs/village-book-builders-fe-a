a3f310f26506aa1749b6a875eb2df0479c0e587f
Paul Lee <paul.lee0627@gmail.com>
2021-03-03 17:20:32 -0800

    set fetchMentorSlots to props, added useEffect and mapStateToProps, set up connect for react-redux, started adding to render

--- src/components/pages/Headmaster/MentorMenteeMatching/MentorSlotView.js
+++ src/components/pages/Headmaster/MentorMenteeMatching/MentorSlotView.js
@@ -1,6 +1,27 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchMentorSlots } from '../../../../state/actions/index';
 
-const MentorSlotView = props => {};
+const MentorSlotView = props => {
+  const { fetchMentorSlots } = props;
 
-export default MentorSlotView;
+  useEffect(() => {
+    fetchMentorSlots();
+  }, [fetchMentorSlots]);
+
+  return (
+    <div className="mentorAvail">
+      <h2>Mentor Availability</h2>
+      <div></div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    isLoading: state.headmasterReducer.isLoading,
+    slot: state.headmasterReducer.slot,
+  };
+};
+
+export default connect(mapStateToProps, { fetchMentorSlots })(MentorSlotView);
