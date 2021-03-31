a680615331e69e68209a62d010c30a7d3736abcb
Paul Lee <paul.lee0627@gmail.com>
2021-03-31 08:31:42 -0700

    removed unnecessary slot code from reducer file

--- src/state/reducers/headmasterReducer.js
+++ src/state/reducers/headmasterReducer.js
@@ -13,14 +13,9 @@ import {
   FETCH_CALENDAR_START,
   FETCH_CALENDAR_SUCCESS,
   FETCH_CALENDAR_FAILURE,
-
-  FETCH_MENTOR_SLOTS_START,
-  FETCH_MENTOR_SLOTS_SUCCESS,
-  FETCH_MENTOR_SLOTS_FAILURE,
   FETCH_TIMESLOTS_START,
   FETCH_TIMESLOTS_SUCCESS,
   FETCH_TIMESLOTS_FAILURE,
-
 } from '../actions/actionTypes';
 
 import { debugLog } from '../../utils/debugMode.js'; //
@@ -41,10 +36,7 @@ const initialState = {
       date: null,
     },
   ],
-
-  slot: [],
   timeSlots: [],
-
 };
 // Fetch school data for headmaster
 const reducer = (state = initialState, action) => {
@@ -105,17 +97,6 @@ const reducer = (state = initialState, action) => {
     case FETCH_CALENDAR_FAILURE:
       debugLog(action.type, action.payload);
       return { ...state, isLoading: false };
-
-    case FETCH_MENTOR_SLOTS_SUCCESS:
-      debugLog(action.type, action.payload);
-      return { ...state, isLoading: false, slot: action.payload };
-    case FETCH_MENTOR_SLOTS_START:
-      debugLog(action.type, action.payload);
-      return { ...state, isLoading: true };
-    case FETCH_MENTOR_SLOTS_FAILURE:
-      debugLog(action.type, action.payload);
-      return { ...state, isLoading: false };
-
     case FETCH_TIMESLOTS_START:
       debugLog(action.type, action.payload);
       return { ...state, isLoading: true };
@@ -129,7 +110,6 @@ const reducer = (state = initialState, action) => {
     case FETCH_TIMESLOTS_FAILURE:
       debugLog(action.type, action.payload);
       return { ...state, isLoading: false };
-
   }
 };
 
